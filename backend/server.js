75a67984e06f28c4629106d815771e98dd25625a
Kirill K <kiryahitya568202@gmail.com>
2026-02-03 17:46:55 +0500

    Update project files

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const SECRET_KEY = process.env.SECRET_KEY;
 const cors = require("cors");
 
 app.use(cors({
-    origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+    origin: ["http://localhost:3000", "http://127.0.0.1:3000","https://tg-miniapp-kwsg.onrender.com"],
     credentials: true
 }));
 
